579710ef2bdf60446fe8db1ac16634f3cb3658f6
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-11-04 14:38:07 +0100

    Add Prettier and Tailwind CSS plugin for code formatting

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,10 @@ import { useState } from "react";
 import Image from "next/image";
 import Header from "./components/Header";
 import Button from "./components/Button";
-import Navy from "./img/navy.png";
-import Green from "./img/green.png";
-import Pink from "./img/pink.png";
-import Watch from "./img/Watch.png";
+import Navy from "/navy.png";
+import Green from "/green.png";
+import Pink from "/pink.png";
+import Watch from "/Watch.png";
 
 export default function Home() {
   const [activeImage, setActiveImage] = useState(Navy);
@@ -15,15 +15,33 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <section>
+      <section className="m-8">
         <h1>The Perfect Moment Between Past And Future</h1>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <Button activeImage={activeImage} setActiveImage={setActiveImage} relatedImage={Green} color="green" />
-          <Button activeImage={activeImage} setActiveImage={setActiveImage} relatedImage={Pink} color="pink" />
-          <Button activeImage={activeImage} setActiveImage={setActiveImage} relatedImage={Navy} color="navy" />
+          <Button
+            activeImage={activeImage}
+            setActiveImage={setActiveImage}
+            relatedImage={Green}
+            color="green"
+          />
+          <Button
+            activeImage={activeImage}
+            setActiveImage={setActiveImage}
+            relatedImage={Pink}
+            color="pink"
+          />
+          <Button
+            activeImage={activeImage}
+            setActiveImage={setActiveImage}
+            relatedImage={Navy}
+            color="navy"
+          />
         </div>
         <figure>
-          <Image src={activeImage} style={{ width: "300px", height: "300px" }} />
+          <Image
+            src={activeImage}
+            style={{ width: "300px", height: "300px" }}
+          />
         </figure>
 
         <div style={{ display: "flex" }}>
